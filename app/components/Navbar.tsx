9099b8ada24ad96f3f369a5626dc9e64f00bafde
Vanessa Hoamea <vanessahoamea@yahoo.com>
2023-10-01 22:10:23 +0300

    fixed navbar markup

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,37 +6,35 @@ import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 export default function Navbar()
 {
     return (
-        <nav className="bg-white mx-auto">
-            <div className="p-4">
-                <div className="px-8 py-4 flex justify-between items-center">
-                    <h2 className="font-bold text-primary text-2xl">Scaffolding</h2>
+        <header className="bg-white mx-auto">
+            <nav className="px-12 py-8 flex justify-between items-center">
+                <h2 className="font-bold text-primary text-2xl">Scaffolding</h2>
 
-                    <div className="hidden md:flex items-center space-x-5">
-                        <Link className="web-menu-link" href="/">Acasă</Link>
-                        <Link className="web-menu-link" href="/about">Despre noi</Link>
-                        <Link className="web-menu-link" href="/services">Servicii</Link>
-                        <Link className="web-menu-link" href="/contact">Contact</Link>
-                        <a 
-                            className="social-media-link"
-                            href="https://www.facebook.com/"
-                            target="_blank"
-                        >
-                            <FontAwesomeIcon icon={faFacebook}/>
-                        </a>
-                    </div>
-
-                    <div className="md:hidden">
-                        <HamburgerMenu />
-                    </div>
+                <div className="hidden md:flex items-center space-x-5">
+                    <Link className="web-menu-link" href="/">Acasă</Link>
+                    <Link className="web-menu-link" href="/about">Despre noi</Link>
+                    <Link className="web-menu-link" href="/services">Servicii</Link>
+                    <Link className="web-menu-link" href="/contact">Contact</Link>
+                    <a 
+                        className="social-media-link"
+                        href="https://www.facebook.com/"
+                        target="_blank"
+                    >
+                        <FontAwesomeIcon icon={faFacebook}/>
+                    </a>
                 </div>
-            </div>
 
-            <div className="mobile-menu hidden">
+                <div className="md:hidden">
+                    <HamburgerMenu />
+                </div>
+            </nav>
+
+            <nav className="mobile-menu hidden md:hidden">
                 <Link href="/">Acasă</Link>
                 <Link href="/about">Despre noi</Link>
                 <Link href="/services">Servicii</Link>
                 <Link href="/contact">Contact</Link>
-            </div>
-        </nav>
+            </nav>
+        </header>
     );
 }
