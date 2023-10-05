c4dd81d12aceb3eb5987b4957c709176d7359d7e
Vanessa Hoamea <vanessahoamea@yahoo.com>
2023-10-05 17:51:41 +0300

    services page draft

---
+++ app/components/AccordionItem.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleMinus, faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import { AnimatePresence, motion } from "framer-motion";
+
+interface AccordionItemProps
+{
+    id: number
+    title: string,
+    content: string,
+    prices: string
+};
+export default function AccordionItem(props: AccordionItemProps)
+{
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggleItem(): void
+    {
+        setIsOpen((isOpen) => !isOpen);
+    }
+    
+    return (
+        <div className="mb-3 last:mb-0">
+            <button
+                className="w-full bg-gray-200 text-xl text-left text-primary font-semibold focus:outline-none rounded-lg shadow-md p-4 flex justify-between items-center"
+                onClick={toggleItem}
+            >
+                <h3>{props.title}</h3>
+                <FontAwesomeIcon icon={isOpen ? faCircleMinus : faCirclePlus} />
+            </button>
+            <AnimatePresence>
+            {
+                isOpen &&
+                <motion.div
+                    initial="collapsed"
+                    animate="open"
+                    exit="collapsed"
+                    variants={{
+                        collapsed: { opacity: 0, height: 0 },
+                        open: { opacity: 1, height: "auto" }
+                    }}
+                    transition={{ duration: 0.25 }}
+                    className="border-l-2 border-l-primary ml-2 px-4"
+                >
+                    <h4 className="mt-3 text-primary font-semibold">Descriere</h4>
+                    <p>{props.content}</p>
+
+                    <h4 className="mt-3 text-primary font-semibold">Tarife</h4>
+                    <p>{props.prices}</p>
+                </motion.div>
+            }
+            </AnimatePresence>
+        </div>
+    );
+}
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,18 @@
-export default function Services()
+import path from "path";
+import fsPromises from "fs/promises";
+import AccordionItem from "../components/AccordionItem";
+
+interface ServiceData
 {
+    id: number,
+    title: string,
+    content: string,
+    prices: string
+};
+export default async function Services()
+{
+    const { data } = await getData();
+
     return (
         <main>
             <div className="h-40 relative overflow-hidden text-white text-center">
@@ -13,7 +26,32 @@ export default function Services()
                 </div>
             </div>
 
-            <p className="p-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga impedit, officiis ipsa aliquid possimus inventore voluptate laborum maxime labore in, soluta illo hic, quis quidem harum fugit. Possimus ex ipsam, placeat necessitatibus error et quas, consequatur beatae ratione voluptatum nostrum, amet delectus. Laboriosam voluptas animi quisquam neque aut quae totam voluptatem nesciunt deleniti temporibus beatae doloremque aspernatur dolor quibusdam natus eveniet recusandae libero, dignissimos pariatur non magnam? Veniam dolore repellat nam nulla assumenda nisi nostrum esse, nemo, aliquam odit aperiam recusandae dolorem dolores itaque impedit quaerat autem aliquid dolorum? Vel, iure consectetur blanditiis natus voluptate maiores neque incidunt totam repellendus.</p>
+            <section className="px-16 pt-20">
+                <h2 className="text-primary text-3xl font-bold">Servicii și produse</h2>
+                <p className="my-2">La Scaffolding Professional Team, suntem dedicați să vă oferim o gamă completă de servicii și produse în domeniul schelelor pentru a satisface nevoile diferite ale clienților noștri. Indiferent de nevoile dvs., suntem aici pentru a vă ajuta să vă atingeți obiectivele de construcție într-un mod sigur și eficient. Prețurile noastre variază în funcție de tipul de serviciu sau produs și de durata închirierii. Iată câteva dintre serviciile noastre comune și prețurile lor orientative:</p>
+                <div className="my-4">
+                {
+                    data.map((service: ServiceData) => (
+                        <AccordionItem key={service.id} {...service} />
+                    ))
+                }
+                </div>
+                <p className="my-2">Ne propunem să oferim cele mai competitive prețuri din industrie și suntem bucuroși să discutăm cu dvs. pentru a vă oferi o ofertă personalizată care să se potrivească nevoilor dvs. Pentru informații detaliate despre tarife sau pentru a primi o ofertă personalizată, vă rugăm să ne contactați. Suntem aici pentru a vă ajuta cu fiecare etapă a proiectului dvs. de construcție.</p>
+            </section>
+
+            <section className="px-16 pt-20 pb-20">
+                <h2 className="text-primary text-3xl font-bold">Portofoliul nostru</h2>
+                <p className="my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem facere tempora doloremque minima consectetur quo nisi veniam pariatur, facilis atque iusto, nam, incidunt totam iure earum dicta blanditiis? Aperiam a libero tempore eaque assumenda molestiae omnis earum. Dolore, consectetur earum.</p>
+            </section>
         </main>
     );
+}
+
+async function getData(): Promise<{ data: ServiceData[] }>
+{
+    const filePath = path.join(process.cwd(), "data/services.json");
+    const jsonData = await fsPromises.readFile(filePath);
+    const objectData = JSON.parse(jsonData.toString());
+
+    return objectData;
 }
