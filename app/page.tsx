8c31ba3a01724cec8cd68183667ff52b485f5f62
Vanessa Hoamea <vanessahoamea@yahoo.com>
2024-03-27 18:24:12 +0200

    added metadata to pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 import { SliceZone } from "@prismicio/react";
@@ -12,4 +13,15 @@ export default async function Home()
             <SliceZone slices={page.data.slices} components={components} />
         </main>
     );
+}
+
+export async function generateMetadata(): Promise<Metadata>
+{
+    const client = createClient();
+    const page = await client.getSingle("home_page");
+  
+    return {
+      title: page.data.meta_title,
+      description: page.data.meta_description
+    };
 }
